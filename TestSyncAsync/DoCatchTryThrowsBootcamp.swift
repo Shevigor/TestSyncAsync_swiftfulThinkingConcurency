cfba96302c921b09f6f5ef11627a65f5f376f9e3
Igor Sheverda <ktgtoit@gmail.com>
2023-07-11 16:21:47 +0300

    4 commit do catch try throws помечаем функцию как throws и в ней используем выброску ошибки throw
    где то используем do catch. в do используем try - обязательный - если ошибка то попадаем в блок catch или - необязательный - try? тогда даже если ошибка в блок catch не идем

--- TestSyncAsync/DoCatchTryThrowsBootcamp.swift
+++ TestSyncAsync/DoCatchTryThrowsBootcamp.swift
@@ -13,7 +13,7 @@ import SwiftUI
 
 class DoCatchTryThrowsBootcampDataManager {
     
-    let isActive: Bool = false
+    private let isActive: Bool = true
     
     func getTitle() -> (title: String?, error: Error?) {
         if isActive {
@@ -25,28 +25,86 @@ class DoCatchTryThrowsBootcampDataManager {
         }
     }
     
+    func getTitle2() -> Result<String, Error> {
+        if isActive {
+            return .success("NEW TEXT")
+        } else {
+            return .failure(URLError(.appTransportSecurityRequiresSecureConnection))
+        }
+    }
+    
+    func getTitle3() throws -> String {
+        if isActive {
+            return "NEW TEXT"
+        } else {
+            throw URLError(.badServerResponse)
+        }
+    }
+
+    func getTitle4() throws -> String {
+        if isActive {
+            return "FINAL TEXT"
+        } else {
+            throw URLError(.cancelled)
+        }
+    }
+    
 }
 
 
 class DoCatchTryThrowsBootcampViewModel: ObservableObject {
     
-    @Published var text: String = "Starting text."
+    @Published var myText: String = "Starting text."
     let manager = DoCatchTryThrowsBootcampDataManager()
     
     func fetchTitle() {
+        /*
         let returnValue = manager.getTitle()
         if let newTitle = returnValue.title {
-            self.text = newTitle
+            self.myText = newTitle
         } else if let error = returnValue.error{
-            self.text = error.localizedDescription
+            self.myText = error.localizedDescription
         }
+        */
+        
+        /*
+        let resultValue = manager.getTitle2()
+        switch resultValue {
+        case .success(let newTitle):
+            self.myText = newTitle
+        case .failure(let error):
+            self.myText = error.localizedDescription
+        }
+        */
+         
+        do {                            // попробуй сделай
+            let resultValue = try manager.getTitle3()
+            self.myText = resultValue
+            
+//            let newResultValue = try? manager.getTitle3()     // необязательный try, в блок catch не идем
+//            if let newResultValue = newResultValue {
+//                self.myText = newResultValue
+//            }
+            
+            let finalValue = try manager.getTitle4()
+            self.myText += finalValue
+            
+        } catch let myError {             // ловим ошибку и даем ей имя
+            self.myText = myError.localizedDescription
+        }
+        
+//        let resultValue = try? manager.getTitle3()
+//        if let resultValue = resultValue {
+//            self.myText = resultValue
+//        }
+        
     }
 }
 
 struct DoCatchTryThrowsBootcamp: View {
     @StateObject private var viewModel = DoCatchTryThrowsBootcampViewModel()
     var body: some View {
-        Text(viewModel.text)
+        Text(viewModel.myText)
             .frame(width: 300, height: 300)
             .background(Color.blue)
             .onTapGesture {
