e419125a7eacc4b4e7bbd443929395d07856fb54
Igor Sheverda <ktgtoit@gmail.com>
2023-07-11 13:23:09 +0300

    Initial Commit

---
+++ TestSyncAsync/TestSyncAsyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestSyncAsyncApp.swift
+//  TestSyncAsync
+//
+//  Created by user on 11.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct TestSyncAsyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
